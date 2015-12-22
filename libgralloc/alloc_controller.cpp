b30596b78cd902e31ce07193483bde235c023df9
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2015-12-21 22:31:52 -0500

    reconfigure ifdef SECURE_MM_HEAP for gralloc/ion usage

--- libgralloc/alloc_controller.cpp
+++ libgralloc/alloc_controller.cpp
@@ -1,6 +1,5 @@
 /*
  * Copyright (c) 2011-2012, The Linux Foundation. All rights reserved.
-
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
@@ -194,9 +193,7 @@ int IonController::allocate(alloc_data& data, int usage)
 {
     int ionFlags = 0;
     int ret;
-#ifndef SECURE_MM_HEAP
     bool nonContig = false;
-#endif
 
     data.uncached = useUncached(usage);
     data.allocType = 0;
@@ -206,17 +203,13 @@ int IonController::allocate(alloc_data& data, int usage)
 
     if(usage & GRALLOC_USAGE_PRIVATE_SYSTEM_HEAP) {
         ionFlags |= ION_HEAP(ION_SYSTEM_HEAP_ID);
-#ifndef SECURE_MM_HEAP
         nonContig = true;
-#endif
     }
 
 #ifndef NO_IOMMU
     if(usage & GRALLOC_USAGE_PRIVATE_IOMMU_HEAP) {
         ionFlags |= ION_HEAP(ION_IOMMU_HEAP_ID);
-#ifndef SECURE_MM_HEAP
         nonContig = true;
-#endif
     }
 #endif
 
@@ -280,17 +273,15 @@ int IonController::allocate(alloc_data& data, int usage)
     {
         ALOGW("Falling back to system heap");
         data.flags = ION_HEAP(ION_SYSTEM_HEAP_ID);
-#ifndef SECURE_MM_HEAP
         nonContig = true;
-#endif
         ret = mIonAlloc->alloc_buffer(data);
     }
 
     if(ret >= 0 ) {
         data.allocType |= private_handle_t::PRIV_FLAGS_USES_ION;
-#ifdef SECURE_MM_HEAP
         if(nonContig)
             data.allocType |= private_handle_t::PRIV_FLAGS_NONCONTIGUOUS_MEM;
+#ifndef SECURE_MM_HEAP
         if(ionFlags & ION_SECURE)
             data.allocType |= private_handle_t::PRIV_FLAGS_SECURE_BUFFER;
 #endif
